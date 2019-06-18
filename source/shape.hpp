8e8c7ea7ac346e576ca0749a269a9e2dacb072f6
Hanesto <hanestolp@gmail.com>
2019-06-18 10:51:30 +0200

    box class implemented

--- source/shape.hpp
+++ source/shape.hpp
@@ -1,7 +1,13 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
 class shape
 {
     public:
     virtual float area(void) = 0;
     virtual float volume(void) = 0;
 
+    private:
 };
+
+#endif
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,12 +1,20 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 #include "sphere.hpp"
+#include "box.hpp"
+#include <glm/vec3.hpp>
 
 TEST_CASE("Sphere and Box Shapes")
 {
   sphere s1(5.0f);
   REQUIRE(s1.area() == 314.15927f);
   REQUIRE(s1.volume() == 523.58569f);
+ 
+  glm::vec3 min = {0,0,0};
+  glm::vec3 max = {5,5,2};
+  box b1(min, max);
+  REQUIRE(b1.area() == 90);
+  REQUIRE(b1.volume() == 50);
 }
 
 int main(int argc, char *argv[])
